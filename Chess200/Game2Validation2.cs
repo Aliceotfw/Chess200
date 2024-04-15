9cd6429a3836edd7c3298bcc65a8a5bf97fe89d3
Aliceotfw <152912443+Aliceotfw@users.noreply.github.com>
2024-04-15 19:44:41 +0400

    Update Game2Validation2.cs

--- Chess200/Game2Validation2.cs
+++ Chess200/Game2Validation2.cs
@@ -5,11 +5,7 @@ internal class Game2Validation2
     public void Validate(Coordinates queenCurrent, Coordinates rook1Current, Coordinates rook2Current,
         Coordinates kingBCurrent, Coordinates kingWCurrent)
     {
-
         Game2Board chess = new Game2Board();
-
-
-
         chess.PrintBoard(queenCurrent, rook1Current, rook2Current,
         kingBCurrent, kingWCurrent);
     }
